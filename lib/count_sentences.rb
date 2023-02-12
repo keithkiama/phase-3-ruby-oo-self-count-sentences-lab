af378555168df61f5ecdc45ce8cf8be34aef5dbc
keithkiama <keithkiama2003@gmail.com>
2023-02-12 14:13:59 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-      self.split('.').join('?').split('?').join('!').split('!').delete_if do |sentence| sentence.size < 2 end.size
+      self.split('.').join('?').split('?').join('!').split('!').delete_if do |sentence| sentence.size < 3 end.size
   end
 end
 
